db8649f57be196b5872627f70b89670dbc87d0cb
markti <markti@outlook.com>
2023-10-12 22:55:01 -0700

    public private

--- src/terraform/ssh.tf
+++ src/terraform/ssh.tf
@@ -10,7 +10,7 @@ resource "azurerm_key_vault_secret" "ssh_private_key" {
 
   depends_on = [azurerm_role_assignment.terraform_keyvault]
 }
-resource "azurerm_key_vault_secret" "ssh_private_key" {
+resource "azurerm_key_vault_secret" "ssh_public_key" {
   name         = "ssh-public-key"
   value        = tls_private_key.ssh.public_key_pem
   key_vault_id = azurerm_key_vault.main.id
