c20569e349dd6dcbb782768a8e0c437eb7ae2deb
markti <markti@outlook.com>
2023-10-14 18:26:46 -0400

    outbound rule

--- src/terraform/loadbalancer-frontend.tf
+++ src/terraform/loadbalancer-frontend.tf
@@ -23,6 +23,17 @@ resource "azurerm_lb_backend_address_pool" "frontend" {
   name            = "frontend-pool"
 }
 
+resource "azurerm_lb_outbound_rule" "main" {
+  name                    = "OutboundRule"
+  loadbalancer_id         = azurerm_lb.frontend.id
+  protocol                = "Tcp"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.frontend.id
+
+  frontend_ip_configuration {
+    name = "PublicIPAddress"
+  }
+}
+
 # Connects this Virtual Machine to the Load Balancer's Backend Address Pool
 resource "azurerm_network_interface_backend_address_pool_association" "frontend" {
 
