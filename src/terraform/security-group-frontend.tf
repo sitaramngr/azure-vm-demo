ebaf2aa0bed5f34eabdf369281222a8a354c5ba3
markti <markti@outlook.com>
2023-10-14 19:04:44 -0400

    2006

--- src/terraform/security-group-frontend.tf
+++ src/terraform/security-group-frontend.tf
@@ -27,7 +27,7 @@ resource "azurerm_network_security_rule" "frontend_http_internal" {
   resource_group_name          = azurerm_resource_group.main.name
   network_security_group_name  = azurerm_network_security_group.frontend.name
   name                         = "allow-http-internal"
-  priority                     = "2005"
+  priority                     = "2006"
   access                       = "Allow"
   direction                    = "Inbound"
   protocol                     = "Tcp"
