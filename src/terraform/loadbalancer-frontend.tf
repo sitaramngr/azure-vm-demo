b4a2d78f26503f8470215945cf133a5d78770e02
markti <markti@outlook.com>
2023-10-14 15:09:07 -0400

    update PIP to be standard

--- src/terraform/loadbalancer-frontend.tf
+++ src/terraform/loadbalancer-frontend.tf
@@ -4,6 +4,7 @@ resource "azurerm_public_ip" "frontend_load_balancer" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_lb" "frontend" {
