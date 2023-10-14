544bcc68d376d2e846544355d0481ccf244fb8cf
markti <markti@outlook.com>
2023-10-13 22:47:32 -0400

    40

--- src/terraform/network.tf
+++ src/terraform/network.tf
@@ -6,7 +6,7 @@ resource "azurerm_virtual_network" "main" {
 }
 
 locals {
-  subnet_address_space = cidrsubnet(var.vpc_cidr_block, 6, 6)
+  subnet_address_space = cidrsubnet(var.vpc_cidr_block, 4, 0)
 }
 /*
 resource "azurerm_subnet" "frontend" {
