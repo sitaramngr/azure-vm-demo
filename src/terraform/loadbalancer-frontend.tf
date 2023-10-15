9892449d49123e0f23331cbca6e95d83ed5fa502
markti <markti@outlook.com>
2023-10-15 11:05:08 -0400

    frontend

--- src/terraform/loadbalancer-frontend.tf
+++ src/terraform/loadbalancer-frontend.tf
@@ -1,10 +1,11 @@
 
-resource "azurerm_public_ip" "frontend_load_balancer" {
+resource "azurerm_public_ip" "frontend" {
   name                = "pip-lb-${var.application_name}-${var.environment_name}-frontend"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Static"
   sku                 = "Standard"
+  zones               = [1, 2, 3]
 }
 
 resource "azurerm_public_ip" "frontend_green" {
@@ -24,7 +25,7 @@ resource "azurerm_lb" "frontend" {
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.frontend_green.id
+    public_ip_address_id = azurerm_public_ip.frontend.id
   }
 }
 resource "azurerm_lb_backend_address_pool" "frontend" {
