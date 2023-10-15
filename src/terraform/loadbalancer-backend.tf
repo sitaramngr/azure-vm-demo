06abafc40e2d4ff499d48897c72b9a009b2eb443
markti <markti@outlook.com>
2023-10-14 21:14:35 -0400

    add backend LB

---
+++ src/terraform/loadbalancer-backend.tf
@@ -0,0 +1,58 @@
+
+resource "azurerm_lb" "backend" {
+  name                = "lb-${var.application_name}-${var.environment_name}-backend"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name      = "PrivateIP"
+    subnet_id = azurerm_subnet.backend.id
+  }
+}
+resource "azurerm_lb_backend_address_pool" "backend" {
+  loadbalancer_id = azurerm_lb.backend.id
+  name            = "backend-pool"
+}
+
+resource "azurerm_lb_outbound_rule" "backend" {
+  name                    = "OutboundRule"
+  loadbalancer_id         = azurerm_lb.backend.id
+  protocol                = "Tcp"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.backend.id
+
+  frontend_ip_configuration {
+    name = "PrivateIP"
+  }
+}
+
+# Connects this Virtual Machine to the Load Balancer's Backend Address Pool
+resource "azurerm_network_interface_backend_address_pool_association" "backend" {
+
+  count = var.az_count
+
+  network_interface_id    = azurerm_network_interface.backend[count.index].id
+  ip_configuration_name   = "internal"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.backend.id
+
+}
+
+resource "azurerm_lb_probe" "backend_probe_http" {
+  loadbalancer_id = azurerm_lb.backend.id
+  name            = "http"
+  protocol        = "Http"
+  port            = 5000
+  request_path    = "/health"
+}
+
+resource "azurerm_lb_rule" "backend_http" {
+  loadbalancer_id                = azurerm_lb.backend.id
+  name                           = "HTTP"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 5000
+  frontend_ip_configuration_name = "PrivateIP"
+  probe_id                       = azurerm_lb_probe.backend_probe_http.id
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.backend.id]
+  disable_outbound_snat          = true
+}
--- src/terraform/loadbalancer-frontend.tf
+++ src/terraform/loadbalancer-frontend.tf
@@ -17,18 +17,13 @@ resource "azurerm_lb" "frontend" {
     name                 = "PublicIPAddress"
     public_ip_address_id = azurerm_public_ip.frontend_load_balancer.id
   }
-
-  frontend_ip_configuration {
-    name      = "BackendAPI"
-    subnet_id = azurerm_subnet.backend.id
-  }
 }
 resource "azurerm_lb_backend_address_pool" "frontend" {
   loadbalancer_id = azurerm_lb.frontend.id
   name            = "frontend-pool"
 }
 
-resource "azurerm_lb_outbound_rule" "main" {
+resource "azurerm_lb_outbound_rule" "frontend" {
   name                    = "OutboundRule"
   loadbalancer_id         = azurerm_lb.frontend.id
   protocol                = "Tcp"
