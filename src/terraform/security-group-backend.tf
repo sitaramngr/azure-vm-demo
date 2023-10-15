722ab0653edc2f20ba0c4f13b3ddf9bb08bb3c93
markti <markti@outlook.com>
2023-10-14 21:03:48 -0400

    backend nsg

---
+++ src/terraform/security-group-backend.tf
@@ -0,0 +1,23 @@
+resource "azurerm_network_security_group" "backend" {
+
+  name                = "nsg-${var.application_name}-${var.environment_name}-backend"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+}
+
+resource "azurerm_network_security_rule" "backend_http_internal" {
+
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.backend.name
+  name                        = "allow-http"
+  priority                    = "2001"
+  access                      = "Allow"
+  direction                   = "Inbound"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "5000"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+
+}
