2d21a421c078859fbd87f9ee991e9e6616baa4c2
sitaramngr <120793639+sitaramngr@users.noreply.github.com>
2024-12-30 11:35:56 +0530

    delete

--- src/terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "frontend_public_ip_address" {
-  value = azurerm_public_ip.frontend.ip_address
-}
-
-output "backend_private_ip_address" {
-  value = azurerm_lb.backend.frontend_ip_configuration[0].private_ip_address
-}
--- src/terraform/versions.tf
+++ src/terraform/versions.tf
@@ -10,10 +10,15 @@ terraform {
     }
   }
   backend "azurerm" {
+    resource_group_name  = "1-4c8e4e3a-playground-sandbox"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "sitaram1992"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "terraform"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
 }
 # do be
